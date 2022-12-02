13b14018ce7f881905815bb4ffba046993629fed
PPC <pierre.paum.cavallera@gmail.com>
2022-12-02 14:19:49 +0100

    cleaned structure

---
+++ Algo/Class/DataReader.py
@@ -0,0 +1,42 @@
+def project_creation(path) -> list:
+    """Create all current projects.
+
+    Keyword arguments:
+    path -- file's path for hr needs
+    Return:
+    project's list
+    """
+    pass
+
+def role_creation(path, li_project) -> list:
+    """Create all current roles.
+
+    Keyword arguments:
+    path -- file's path for hr needs
+    li_project -- the list of all cure=rent linked projects
+    Return:
+    role's list
+    """
+    pass
+
+def person_creation(path)-> list:
+    """Create all current person.
+
+    Keyword arguments:
+    path -- file's path for role choices
+    Return:
+    person's list
+    """
+    pass
+
+def choices_creation(path, li_person, li_role)-> list:
+    """Create all current person.
+
+    Keyword arguments:
+    path -- file's path for role choices
+    li_person -- person's list
+    li_role -- list of current linked roles
+    Return:
+    person's list
+    """
+    pass
---
+++ Algo/main.py
@@ -0,0 +1,3 @@
+from Class.DataReader import *
+
+person_creation()
